c72bf77c6a84c8b55843e040a5d842db143bc07e
Samuel DeSantis <samuel.i.desantis@gmail.com>
2020-12-10 04:07:45 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,24 @@
 class Transfer
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+  end
 
+  def valid?
+    @sender.status == "open" && @receiver.status == "open"
+  end
+
+  def execute_transaction
+    if @sender.balance >= @amount
+      @sender.balance -= @amount
+      @receiver.balance += @amount
+    else
+      puts "ERROR #=> Balance exceeds withdraw amount."
+    end
+  end
+
+  def reverse_transfer
+  end
 end
